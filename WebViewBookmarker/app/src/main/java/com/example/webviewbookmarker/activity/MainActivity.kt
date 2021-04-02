40b81930dbfd3c56cf7f109893529660ad51e87c
arutoria <arutoria000@gmail.com>
2021-04-02 15:29:26 +0900

    no message

---
+++ WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.webviewbookmarker.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.webviewbookmarker.R
+
+/**
+ * メインActivity.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/TopActivity.kt
@@ -0,0 +1,16 @@
+package com.example.webviewbookmarker.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.webviewbookmarker.R
+
+/**
+ * トップ画面.
+ */
+class TopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top)
+    }
+}
