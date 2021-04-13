8066941913f5ad8da0d970d8125ac1a92ca12a71
ebid <ebid@systena.co.jp>
2021-04-13 12:26:20 +0900

    CustomTabsWebViewを導入

--- WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/TopActivity.kt
+++ WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/TopActivity.kt
@@ -1,6 +1,10 @@
 package com.example.webviewbookmarker.activity
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.webviewbookmarker.R
 
@@ -11,6 +15,18 @@ class TopActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // view設定
         setContentView(R.layout.activity_top)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+
+        val view : View = findViewById(R.id.fab_search_button)
+        view.setOnClickListener{
+            val intent = Intent(this, CustomTabsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
