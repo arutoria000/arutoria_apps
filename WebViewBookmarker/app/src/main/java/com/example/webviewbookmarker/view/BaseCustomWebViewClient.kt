c21ffaa5c132215cb9a5e6634b707d7467761622
ebid <ebid@systena.co.jp>
2021-04-20 15:08:15 +0900

    Top画面のブックマークレイアウトを作成

--- WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/view/BaseCustomWebViewClient.kt
+++ WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/view/BaseCustomWebViewClient.kt
@@ -4,17 +4,40 @@ import android.graphics.Bitmap
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import org.jetbrains.annotations.NotNull
 
-class BaseCustomWebViewClient: WebViewClient() {
+class BaseCustomWebViewClient(@NotNull onPageLoadStateChangeListener: OnPageLoadStateChangeListener): WebViewClient() {
+
+    /**
+     * WebViewのページロードステータス変更検知リスナー.
+     */
+    interface OnPageLoadStateChangeListener {
+        /**
+         * ページロード開始.
+         */
+        fun onPageLoadStart()
+
+        /**
+         * ページロード終了.
+         */
+        fun onPageLoadFinish()
+    }
+
+    /**
+     * ページロードステータス変更検知リスナー.
+     */
+    private val mOnPageLoadStateChangeListener: OnPageLoadStateChangeListener = onPageLoadStateChangeListener
 
     override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
         super.onPageStarted(view, url, favicon)
-        Log.d("##!#", "onPageStarted URL = " + url)
+        // 開始通知
+        mOnPageLoadStateChangeListener.onPageLoadStart()
     }
 
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
-        Log.d("##!#", "onPageFinished URL = " + url)
+        // 終了通知
+        mOnPageLoadStateChangeListener.onPageLoadFinish()
     }
 
 }
