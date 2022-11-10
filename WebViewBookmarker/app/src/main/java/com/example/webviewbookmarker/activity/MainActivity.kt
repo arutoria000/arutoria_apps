76a0cb4035d52caeb655cc80aab2c4a44c221aed
arutoria <arutoria000@gmail.com>
2022-11-10 23:12:17 +0900

    step0 初期設定

--- WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/MainActivity.kt
+++ WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.webviewbookmarker.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.webviewbookmarker.R
 
 /**
  * メインActivity.
  */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // トップ画面を起動
+        val intent = Intent(this, TopActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/TopActivity.kt
+++ WebViewBookmarker/app/src/main/java/com/example/webviewbookmarker/activity/TopActivity.kt
@@ -7,10 +7,11 @@ import com.example.webviewbookmarker.R
 /**
  * トップ画面.
  */
-class TopActivity : AppCompatActivity() {
+class TopActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // View設定
         setContentView(R.layout.activity_top)
     }
 }
